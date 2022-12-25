26354f808c77105fe26f14545d09364735b44e86
Navid Golforoushan <ngolforoushan@gmail.com>
2022-12-25 14:17:01 -0700

    Updated startup

--- src/YADA.WebApi/Startup.cs
+++ src/YADA.WebApi/Startup.cs
@@ -7,22 +7,25 @@ namespace YADA.WebApi
 {
     public static class Startup
     {
+        private static readonly string OUTPUT_TEMPLATE = "[{Timestamp:HH:mm:ss} {Level:u3}] {ConnectionId} {Message:lj}{NewLine}{Exception}";
+
         public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
         {
             var logger = new LoggerConfiguration()
                 .ReadFrom.Configuration(builder.Configuration)
                 .Enrich.FromLogContext()
-                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {ConnectionId} {Message:lj}{NewLine}{Exception}")
+                .WriteTo.Console(outputTemplate: OUTPUT_TEMPLATE)
                 .CreateLogger();
             builder.Logging.ClearProviders();
             builder.Logging.AddSerilog(logger);
 
-            logger.Information("Starting web application");
             builder.Services.AddHttpContextAccessor();
-            builder.Services.AddControllers(o => o.ReturnHttpNotAcceptable = true)
+            builder.Services.AddControllers(o =>
+                o.ReturnHttpNotAcceptable = true)
                 .AddXmlSerializerFormatters();
             builder.Services.AddScoped<IProfileReporsitory, ProfileRepository>();
-            builder.Services.AddDbContext<ApplicationContext>(o => o.UseSqlite(@"Data Source=database.db"));
+            builder.Services.AddDbContext<ApplicationContext>(o =>
+                o.UseSqlite(@"Data Source=database.db"));
             builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             return builder.Build();
@@ -30,6 +33,9 @@ namespace YADA.WebApi
 
         public static WebApplication ConfigurePipeline(this WebApplication app)
         {
+            Console.Title = $"YADA-WebApi ({app.Environment.EnvironmentName})";
+            Log.Information($"Starting web application");
+
             if (app.Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
