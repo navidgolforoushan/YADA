f4f72196ce9a472e3f77f870e0291bdaa42b6396
Navid Golforoushan <ngolforoushan@gmail.com>
2022-12-25 10:51:00 -0700

    Updated navigation controller

--- src/YADA.WebApi/Controllers/ProfileController.cs
+++ src/YADA.WebApi/Controllers/ProfileController.cs
@@ -5,6 +5,8 @@ using YADA.WebApi.Services;
 
 namespace YADA.WebApi.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class ProfileController : Controller
     {
         private readonly IProfileReporsitory _profileReporsitory;
@@ -20,15 +22,15 @@ namespace YADA.WebApi.Controllers
                 throw new ArgumentException(nameof(mapper));
         }
 
-        [HttpPost("api/profile")]
-        public async Task<ActionResult<IEnumerable<ProfileDto>>> GetProfiles()
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProfileDto>>> Get()
         {
             var profilesFromRepo = await _profileReporsitory.GetProfilesAsync();
             return Ok(_mapper.Map<IEnumerable<ProfileDto>>(profilesFromRepo));
         }
 
-        [HttpGet("api/getprofile/{profileId}", Name = "GetProfile")]
-        public async Task<ActionResult<ProfileDto>> GetProfile(Guid profileId)
+        [HttpGet("{profileId:guid}")]
+        public async Task<ActionResult<ProfileDto>> Get(Guid profileId)
         {
             var profileFromRepo = await _profileReporsitory.GetProfileByIdAsync(profileId);
             return Ok(_mapper.Map<ProfileDto>(profileFromRepo));
