a4a260727ebfc30077f16bdd4df29d8952faa514
Navid Golforoushan <ngolforoushan@gmail.com>
2022-12-25 10:51:12 -0700

    Fixed bug

--- src/YADA.WebApi/Profiles/ProfileProfile.cs
+++ src/YADA.WebApi/Profiles/ProfileProfile.cs
@@ -6,7 +6,7 @@ namespace YADA.WebApi.Profiles
         public ProfileProfile()
         {
             CreateMap<Entities.Profile, Models.ProfileDto>()
-                .ForMember(des => des.Age, opt => opt.MapFrom(src => (src.DateOfBirth.ToDateTime(new TimeOnly()) - DateTime.Now).TotalDays / 365.00))
+                .ForMember(des => des.Age, opt => opt.MapFrom(src => (DateTime.Now - src.DateOfBirth.ToDateTime(new TimeOnly())).TotalDays / 365.00))
                 .ForMember(des => des.FUllName, opt => opt.MapFrom(src => $"{src.FirstName}  {src.LastName}"));
         }
     }
